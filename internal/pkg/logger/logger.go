55f3c06bf05276c7a0bde8bb7469825ff3d266bf
kit-lol <nikita.vitcenko42344@gmail.com>
2026-07-17 22:13:57 +0300

    chore: add slog logger

---
+++ internal/pkg/logger/logger.go
@@ -0,0 +1,42 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+// SetupLogger initializes slog based on the current environment
+func SetupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		// For local development: text format, Debug level
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		// For the test server: JSON format, Debug level
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		// For prod: JSON format, info level (Debug is ignored to save space)
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default:
+		// Folback just in case
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
